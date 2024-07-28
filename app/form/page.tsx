9db5fd6b02456789d0db9d34affc9def8473c68e
Jay Keraliya <jaykeraliya0@gmail.com>
2024-07-28 17:36:24 +0530

    fixed redirection error

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -275,7 +275,7 @@ export default function Page() {
   //   setErrors(errors);
   // }, [data, json]);
 
-  if (!json || !localStorage) return redirect("/create-form");
+  if (!json) return redirect("/create-form");
 
   return (
     <div className="py-16 w-full flex justify-center items-center">
